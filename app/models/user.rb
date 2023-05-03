7821539d32bf2e5c10ec780b83bc42471149cb38
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-03 19:03:52 +0300

    Modify model classes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    self.primary_key = 'author_id'
     has_many :posts, foreign_key: :author_id
     has_many :comments, through: :posts
     has_many :likes, through: :posts
