fb303aefc7c44c83cc653e530c0e1be44357a9d9
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-03 19:17:12 +0300

    Modify model classes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
     belongs_to :user
-    belongs_to :author, classname: 'User', foreign_key: :author_id
+    belongs_to :author, class_name: 'User', foreign_key: :author_id
 end
