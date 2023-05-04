bf189dc3e70f26897993db3a3e8500e918703b62
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-04 13:47:42 +0300

    Add `dependent: :destroy` to parent classes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: :author_id
+    has_many :posts, foreign_key: :author_id, dependent: :destroy
     has_many :comments, through: :posts
     has_many :likes, through: :posts
 
