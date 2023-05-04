232b623cfd03841739e15300cba645a215507d60
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-04 13:34:11 +0300

    Add `update_posts_counter` method for `Comment`

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
     belongs_to :post
     belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+    after_create :update_posts_counter
+
+    def update_posts_counter
+        post.increment!(:comments_counter)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
     has_many :comments
     has_many :likes
 
-    after_create :increment_counter
+    after_create :update_users_counter
 
     def recent_five
         comments.order(created_at: :desc).limit(5)
