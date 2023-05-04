51b51032634d1f39cd3d58d9f54f30f90b58138e
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-04 21:43:13 +0300

    Fix linters

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,36 +6,36 @@ describe User, type: :model do
 
   describe 'validations' do
     it 'name should be present' do
-        subject.name = nil  
-        expect(subject).to_not be_valid
+      subject.name = nil
+      expect(subject).to_not be_valid
     end
     it 'posts_counter should be greater than or equal to 0' do
-        subject.posts_counter = -1  
-        expect(subject).to_not be_valid
+      subject.posts_counter = -1
+      expect(subject).to_not be_valid
     end
     it 'posts_counter should be an integer' do
-        subject.posts_counter = 1.5  
-        expect(subject).to_not be_valid
+      subject.posts_counter = 1.5
+      expect(subject).to_not be_valid
     end
   end
   describe '#initialize' do
     it 'should be valid' do
-        expect(subject).to be_valid
+      expect(subject).to be_valid
     end
     it 'should have a name' do
-        expect(subject.name).to eq('Soap McTavish')
+      expect(subject.name).to eq('Soap McTavish')
     end
     it 'should have a photo' do
-        expect(subject.photo).to eq('Soap_MacTavish.jpg')
+      expect(subject.photo).to eq('Soap_MacTavish.jpg')
     end
     it 'should have a bio' do
-        expect(subject.bio).to eq('Spec-Ops')
+      expect(subject.bio).to eq('Spec-Ops')
     end
   end
   describe '#recent_three' do
     it 'should return up to three of the most recent posts' do
-        post.save
-        expect(subject.recent_three).to eq([post])
+      post.save
+      expect(subject.recent_three).to eq([post])
     end
   end
 end
