21491c3ccf98695abfad518273f0ce2ca60ecdc6
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-04 13:27:57 +0300

    Add `increment_counter` method for `Post` class

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,13 @@ class Post < ApplicationRecord
     has_many :comments
     has_many :likes
 
+    after_create :increment_counter
+
     def recent_five
         comments.order(created_at: :desc).limit(5)
+    end
+
+    def increment_counter
+        author.increment!(:posts_counter)
     end
 end
