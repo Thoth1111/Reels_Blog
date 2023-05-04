dc7ed3e9ab68bb208a3b2e803b2e3f423d54c09e
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-04 19:34:19 +0300

    Add unit tests for `User`

--- db/migrate/20230503082932_add_posts_count_to_users.rb
+++ db/migrate/20230503082932_add_posts_count_to_users.rb
@@ -1,5 +1,5 @@
 class AddPostsCountToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :posts_counter, :integer
+    add_column :users, :posts_counter, :integer, default: 0
   end
 end
