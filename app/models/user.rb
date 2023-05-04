5bd055700a787dab3fad3522cf4554cd3ef2c9b4
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-04 13:14:19 +0300

    Add `recent_three` method to `User` class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :posts, foreign_key: :author_id
     has_many :comments, through: :posts
     has_many :likes, through: :posts
+
+    def recent_three
+        posts.order(created_at: :desc).limit(3)
+    end
 end
