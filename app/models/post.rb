b7aff10c1ff61dd03f0841154f50003b78247ad4
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-04 13:24:02 +0300

    Add `recent_five` method to `Post` class

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: :author_id
     has_many :comments
     has_many :likes
+
+    def recent_five
+        comments.order(created_at: :desc).limit(5)
+    end
 end
