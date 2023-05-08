8d4eb2e9a49eabf3a7c820d0f7ef04aed0020018
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-08 18:39:58 +0300

    Add routes to `users` and `posts`

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :show, :create, :update, :destroy] do
+    resources :posts, only: [:index, :show, :create, :update, :destroy]
+  end
 end
