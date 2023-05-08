1c77fe2d272ab46909cc10ec40c9f3285e8beb2d
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-08 20:42:40 +0300

    Define users index as root in `routes.rb`

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users, only: [:index, :show, :create, :update, :destroy] do
-    resources :posts, only: [:index, :show, :create, :update, :destroy]
+  root 'users#index'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
 end
