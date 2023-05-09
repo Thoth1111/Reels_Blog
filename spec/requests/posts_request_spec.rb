fb187864149b3a9e07c42791080041d8722af0af
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-09 20:38:26 +0300

    Add `show` tests for `posts_controller`

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,24 +1,42 @@
 require 'rails_helper'
 
-describe "Posts", type: :request do
-let(:user) { User.create(name: "Neo", photo: "Neo.jpg", bio: "The one")}
-let(:post) { Post.create(author: user, title: "Awakening", text: "There is no spoon")}
+describe 'Posts', type: :request do
+  let(:user) { User.create(name: 'Neo', photo: 'Neo.jpg', bio: 'The one') }
+  let(:post) { Post.create(author: user, title: 'Awakening', text: 'There is no spoon') }
 
-    describe "GET #index" do
-        before do
-            get user_posts_path(user)
-        end
-
-        it "returns correct response status" do
-            expect(response).to have_http_status(:success)
-        end
-
-        it "renders the index template" do
-            expect(response).to render_template(:index)
-        end
-
-        it "contains the correct placeholder text" do
-            expect(response.body).to include("Here is a list of posts by a specific user by id:")
-        end
+  describe 'GET #index' do
+    before do
+      get user_posts_path(user)
     end
+
+    it 'returns correct response status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'contains the correct placeholder text' do
+      expect(response.body).to include('Here is a list of posts by a specific user by id:')
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      get user_post_path(user, id: post.id)
+    end
+
+    it 'returns correct response status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'contains the correct placeholder text' do
+      expect(response.body).to include('Here are the details of a specific post')
+    end
+  end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,41 +1,41 @@
 require 'rails_helper'
 
-describe "Users", type: :request do
-    describe "GET #index" do
-        before do
-            get users_path
-        end
-
-        it "returns correct response status" do
-            expect(response).to have_http_status(:success)
-        end
-
-        it "renders the index template" do
-            expect(response).to render_template(:index)
-        end
-
-        it "contains the correct placeholder text" do
-            expect(response.body).to include("Here is a list of users:")
-        end
+describe 'Users', type: :request do
+  describe 'GET #index' do
+    before do
+      get users_path
     end
 
-    describe "GET #show" do
-        let(:user) { User.create(name: "Neo", photo: "Neo.jpg", bio: "The one") }
-
-        before do
-            get user_path(id: user.id)
-        end
-
-        it "returns correct response status" do
-            expect(response).to have_http_status(:success)
-        end
-
-        it "renders the show template" do
-            expect(response).to render_template(:show)
-        end
-
-        it "contains the correct placeholder text" do
-            expect(response.body).to include("Here are the details of a specific user by id: ")
-        end
+    it 'returns correct response status' do
+      expect(response).to have_http_status(:success)
     end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'contains the correct placeholder text' do
+      expect(response.body).to include('Here is a list of users:')
+    end
+  end
+
+  describe 'GET #show' do
+    let(:user) { User.create(name: 'Neo', photo: 'Neo.jpg', bio: 'The one') }
+
+    before do
+      get user_path(id: user.id)
+    end
+
+    it 'returns correct response status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'contains the correct placeholder text' do
+      expect(response.body).to include('Here are the details of a specific user by id: ')
+    end
+  end
 end
