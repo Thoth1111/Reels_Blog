6b5e73c7c8f24c7b859500fd5d2eb70bbae97316
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-12 15:20:48 +0300

    render `like` button

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
     def create
-        @comment = Comment.new(comment_params)
+        @comment = current_user.comments.new(comment_params)
         @comment.post_id = params[:post_id]
 
         if @comment.save
