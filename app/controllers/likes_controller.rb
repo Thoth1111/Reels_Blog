ee83e7de54d16fb4c112c9a687ac96300bfdd17e
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-12 12:39:48 +0300

    Create `likes` controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,12 @@
+class LikesController < ApplicationController
+    def create
+        @like = current_user.likes.new
+        @like.post_id = params[:post_id]
+        if @like.save
+            flash[:success] = "You liked this post"
+            redirect_to user_posts_path(current_user, @like.post)
+        else
+            flash.now[:error] = "like failed"
+        end
+    end
+end
