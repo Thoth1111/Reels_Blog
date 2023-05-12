e4fcd5d55963acb56009ed510f359ce8f9b9a6c4
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-12 23:27:14 +0300

    Modify `comments` routing & functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,20 @@
 class CommentsController < ApplicationController
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+  end
 
   def new
     @comment = Comment.new
+    @post = Post.find(params[:post_id])
   end
-  
+
   def create
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = params[:post_id]
 
     if @comment.save
-      redirect_to user_post_path(current_user, @comment.post)
+      redirect_to user_post_comments_path(current_user, @comment.post_id)
     else
       render :create
     end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create] 
+      resources :comments, only: [:index, :new, :create] 
       resources :likes, only: [:create]
     end
   end
 end
-
