e6f9c00ae56ecbbc781ceeb7e0d34d53a067f2e5
Mukama Steven <stevenmukama1@gmail.com>
2023-05-19 08:59:41 +0200

    Add destroy path & comment delete button

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Defines the root path route ("/")
   root 'users#index'
 
-  resources :users, only: [:index, :show] do 
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:index, :new, :create] 
+      resources :comments, only: [:index, :new, :create, :destroy]
       resources :likes, only: [:create,]
     end
   end
