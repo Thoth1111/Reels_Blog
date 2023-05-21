37da8ec198a4e0e616e4486b12613a44e1adfeca
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-21 12:53:31 +0300

    update `routes.rb`

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json} do
     namespace :v1 do
-      resources :users, only: [:index] do
+      resources :users, only: [:index, :create, :destroy] do
         resources :posts, only: [:index, :create, :destroy] do
           resources :comments, only: [:index, :create, :destroy]
           resources :likes, only: [:index, :create, :destroy]
