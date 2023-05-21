246b3693b0fd580abd39e29ff8f688f73c3f2666
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-21 12:32:06 +0300

    Update API  `posts_controller.rb`

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -7,6 +7,32 @@ module Api
         @posts = Post.all
         render json: @posts
       end
+
+      def create
+        @post = Post.new(post_params)
+        @post.user_id = current_user.index
+        if @post.save
+          render json: @post
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        if @post.destroy
+          head :no_content, status: :ok
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def post_params
+        params.require(:post).permit(:title, :text)
+      end
     end
   end
 end
